d6054aa14665f548bb62dae66d315b262b1a4bac
Mark Ducommun and Rodolfo Sanchez <pivotal@Trumbull.local>
2016-10-27 19:17:38 -0500

    randomize test application port, refactor some functionality test functionality into an abstract class

--- src/test/java/io/pivotal/chicago/contracts/example/TodosControllerTest.java
+++ src/test/java/io/pivotal/chicago/contracts/example/TodosControllerTest.java
@@ -1,25 +1,16 @@
 package io.pivotal.chicago.contracts.example;
 
-import au.com.dius.pact.provider.junit.PactRunner;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.loader.PactFolder;
-import au.com.dius.pact.provider.junit.target.HttpTarget;
-import au.com.dius.pact.provider.junit.target.Target;
-import au.com.dius.pact.provider.junit.target.TestTarget;
-import org.junit.Before;
-import org.junit.runner.RunWith;
-import org.springframework.boot.SpringApplication;
 
-@RunWith(PactRunner.class)
+
 @Provider("TodoProviderService")
 @PactFolder("src/test/resources/contracts")
-public class TodosControllerTest {
+public class TodosControllerTest extends ProviderTest {
 
-    @Before
-    public void setUp() throws Exception {
-        SpringApplication.run(TodoProviderServiceApplication.class);
+    @Override
+    public Class<?> getApplicationClass() {
+
+        return TodoProviderServiceApplication.class;
     }
-
-    @TestTarget
-    public final Target target = new HttpTarget(8080);
 }
